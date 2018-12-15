515f8e42848c7e671bb17bc19f8552d6a7d356f4
Eko Prasetyo Kurniawan <ekop22496@gmail.com>
2018-12-15 19:46:48 +0700

    15/12/18

--- application/controllers/Transaksi.php
+++ application/controllers/Transaksi.php
@@ -9,19 +9,15 @@ class Transaksi extends REST_Controller {
 
     // show data transaksi
     function index_get() {
-        $id_transaksi = $this->get('id_transaksi');
         $kode_parkiran = $this->get('kode_parkiran');
-        if ($id_transaksi == '') {
+        
             $this->db->select('*');
 			$this->db->from('parkir_transaksi');
 			$this->db->join('parkir', 'parkir_transaksi.no_karcis = parkir.no_karcis');
 			$this->db->where('kode_parkiran', $kode_parkiran);
 			$this->db->where('status', '2');
 			$transaksi = $this->db->get()->result();
-        } else {
-            $this->db->where('id_transaksi', $id_transaksi);
-            $transaksi = $this->db->get('parkir_transaksi')->result();
-        }
+        
         $this->response(array("result"=>$transaksi, "status"=>"success"));
     }
 
